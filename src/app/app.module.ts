aa54df2ebe246863aa628c954aee44d96fc94b55
BegaDeveloper <ismarbegicit@gmail.com>
2022-05-23 10:29:01 +0200

    Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
@@ -14,6 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/framework/framework.component.ts
+++ src/app/framework/framework.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild,OnInit } from "@angular/core";
+import { FreeMode } from "swiper";
+import { SwiperComponent } from "swiper/angular";
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([FreeMode, Pagination]);
 
 @Component({
   selector: 'app-framework',
   templateUrl: './framework.component.html',
-  styleUrls: ['./framework.component.css']
+  styleUrls: ['./framework.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class FrameworkComponent implements OnInit {
 
